48782b051ce3cc0ca254318beb76944a60e3eefc
ACARPAN <123221549+ACARPAN@users.noreply.github.com>
2023-04-07 19:58:55 -0700

    added

--- Selenium13.java
+++ Selenium13.java
@@ -31,20 +31,56 @@ public class Selenium13 {
 //		String textAccept = driver.findElement(By.cssSelector("#confirm-alert-text")).getText();
 //		System.out.println(textAccept);
 //		driver.quit();
-		
+
 		// confirm() - dismiss()
-		
+
+//		System.setProperty("webdrive.chrome.driver", "C:\\Users\\offic\\OneDrive\\Desktop\\Chrome.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("http://www.webdriveruniversity.com/Popup-Alerts/index.html");
+//		driver.manage().window().maximize();
+//		driver.get("http://www.webdriveruniversity.com/Popup-Alerts/index.html");
+//		driver.findElement(By.cssSelector("#button4")).click();
+//		driver.switchTo().alert().dismiss();
+//		String textDismiss = driver.findElement(By.cssSelector("#confirm-alert-text")).getText();
+//		System.out.println(textDismiss);
+
+		// Program 4 ... Prompt .accept()
+
+//		System.setProperty("webdrive.chrome.driver", "C:\\Users\\offic\\OneDrive\\Desktop\\Chrome.exe");
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.manage().window().maximize();
+//		driver.findElement(By.cssSelector("button[onclick=\"jsPrompt()\"]")).click();
+//		driver.switchTo().alert().sendKeys("Word");
+//		driver.switchTo().alert().accept();
+//		String textValue = driver.findElement(By.id("result")).getText();
+//		if (textValue.equals("You entered: Word")) {
+//			System.out.println("Testcase pass");
+//		} else {
+//			System.out.println("Testcase Fail");
+//		}
+//
+//		Thread.sleep(3000);
+//		driver.close();
+
 		System.setProperty("webdrive.chrome.driver", "C:\\Users\\offic\\OneDrive\\Desktop\\Chrome.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.webdriveruniversity.com/Popup-Alerts/index.html");
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
 		driver.manage().window().maximize();
-		driver.get("http://www.webdriveruniversity.com/Popup-Alerts/index.html");
-		driver.findElement(By.cssSelector("#button4")).click();
+		driver.findElement(By.cssSelector("button[onclick=\"jsPrompt()\"]")).click();
+		//driver.switchTo().alert().sendKeys(" ");
 		driver.switchTo().alert().dismiss();
-		String textDismiss = driver.findElement(By.cssSelector("#confirm-alert-text")).getText();
-		System.out.println(textDismiss);
+		String textNull = driver.findElement(By.id("result")).getText();
+
+		if (textNull.equals("You entered: null")) {
+			System.out.println("Test case pass");
+			
+		} else {
+			System.out.println("Test case fail");
+		}
 		Thread.sleep(3000);
 		driver.close();
+
 	}
 
 }
